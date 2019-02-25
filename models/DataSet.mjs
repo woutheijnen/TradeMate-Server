bace9df960a6648a652e70db5e9ac82082664658
Wout Heijnen <wheijnen.externe@altima-assurances.fr>
2019-02-25 12:26:34 +0100

    Make each dataSet schema have only 1 set

--- models/DataSet.mjs
+++ models/DataSet.mjs
@@ -7,32 +7,25 @@ const Schema = mongoose.Schema;
 const DataSetSchema = new Schema({
   // Link the datafetcher
   dataFetcher: { type: Schema.Types.ObjectId, ref: "data-fetchers", required: true },
-  sets: {
+  set: {
+    type: {
+      first: { type: String, required: true },
+      second: { type: String, required: true }
+    },
+    required: true
+  },
+  candles: {
     type: [
       {
-        set: {
-          type: {
-            first: { type: String, required: true },
-            second: { type: String, required: true },
-            lastUpdate: { type: Date, required: true }
-          },
-          required: true
-        },
-        candles: {
-          type: [
-            {
-              time: { type: Number, required: true },
-              close: { type: Number, required: true },
-              vol: { type: Number, required: true }
-            }
-          ],
-          required: true,
-          default: []
-        }
+        time: { type: Number, required: true },
+        close: { type: Number, required: true },
+        vol: { type: Number, required: true }
       }
     ],
-    required: true
-  }
+    required: true,
+    default: []
+  },
+  lastUpdate: { type: Date, required: true }
 });
 
 export default model("datasets", DataSetSchema);
